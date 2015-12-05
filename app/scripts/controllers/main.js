89cab33dd52b3757503d3d2c002f94140cda6374
George Stefanis <info@georgestefanis.com>
2015-12-05 21:32:15 +0000

    test - adding an http call to google API

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,6 +11,20 @@ angular.module('mainApp')
     .controller('MainCtrl', function($scope, $http) {
         $scope.message = 'Hello World!';
         
+        $http({
+            method : 'GET',
+            url : 'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyC0aV6tMNaki-zrNaq7Ch9EwaSgX1gueuo'
+        }).then(function successCallback(response) {
+            // this callback will be called asynchronously
+            // when the response is available
+            console.log(response);
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+            console.log('error');
+            console.log(response);
+        });
+        
         function initializeMap() {
             var mapCanvas = document.getElementById('map');
             var mapOptions = {
