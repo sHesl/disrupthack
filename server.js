1329907d8a4e9244026fe71f03863db53e21d917
sHesl <samuel.heslop.dev@gmail.com>
2015-12-05 15:16:19 +0000

    Sending text to George

--- server.js
+++ server.js
@@ -8,5 +8,14 @@ app.get('/', function(req, res) {
 	res.send('Hello world');
 }); 
 
+
+app.get('/textGeorge', function(req, res) {
+	twilio.sendText('+447756068326', 'hello George :)');
+}); 
+
+app.get('/twilio', function(req, res){
+	twilio.recieveText();
+});
+
 app.listen(3000);
 console.log('Listening on port 3000...');
