28647d16233ff76a2d2f77bdf7ad5f574eef583e
George Stefanis <info@georgestefanis.com>
2015-12-05 21:12:32 +0000

    adding google maps API

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,4 +10,16 @@
 angular.module('mainApp')
     .controller('MainCtrl', function($scope, $http) {
         $scope.message = 'Hello World!';
+        
+        function initializeMap() {
+            var mapCanvas = document.getElementById('map');
+            var mapOptions = {
+                center: new google.maps.LatLng(44.5403, -78.5463),
+                zoom: 8,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            var map = new google.maps.Map(mapCanvas, mapOptions);
+        }
+        
+        initializeMap();
     });
