b54492558cc8e8bccba47de50f2b789aaf4d9742
sHesl <samuel.heslop.dev@gmail.com>
2015-12-05 15:31:54 +0000

    Listening wip

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 var express = require('express'),
+	bodyparser = require('body-parser'),
     main = require('./server/main'),
     twilio = require('./server/twilio');
 
 var app = express();
 
+app.use(bodyparser.json);
+app.use(bodyparser.urlencoded({ extended: true }));
+
 app.get('/', function(req, res) {
 	res.send('Hello world');
 }); 
-
 
 app.get('/textGeorge', function(req, res) {
 	twilio.sendText('+447756068326', 'hello George :)');
 }); 
 
-app.get('/twilio', function(req, res){
-	twilio.recieveText();
+app.post('/twilio', function(req, res) {
+	twilio.recieveText(req, res);
 });
 
 app.listen(3000);
