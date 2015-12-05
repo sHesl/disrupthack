57b281d7292fce7400f8eabef72fee8c58ece39f
George Stefanis <info@georgestefanis.com>
2015-12-05 19:47:07 +0000

    Adding back front-end stuff on the server

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
     watson = require('./server/watson'),
+    main = require('./server/main'),
+    path = require('path'),
     twilio = require('twilio');
 
 var app = express();
@@ -9,6 +11,11 @@ app.use(bodyParser.json());
 process.env.TWILIO_ACCOUNT_SID = 'AC5a7faa72917a998b94a7eaa2e35b76b3';
 process.env.TWILIO_AUTH_TOKEN = '7c05dff31faa2c7cc957a3e57526b99e';
 
+// serve up static assets
+app.use(express.static(path.join(__dirname, '/app')));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+
+//API
 app.get('/', function(req, res) {
     res.send(process.env.TWILIO_ACCOUNT_SID + ' / ' + process.env.TWILIO_AUTH_TOKEN);
 });
@@ -16,7 +23,7 @@ app.get('/', function(req, res) {
 app.post('/twilio', function(request, response) {
      
     var twiml = new twilio.TwimlResponse();
-    console.log(request.body)
+    console.log(request.body);
     watson.getHealthCareAdvice(request.body, function(result) {
         twiml.message(result);
 
