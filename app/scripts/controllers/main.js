596630053dff04acda52faf9bdb6d2589dc7b5f9
George Stefanis <info@georgestefanis.com>
2015-12-05 23:30:23 +0000

    creating a helper function to get the coords and also adding a popup box on clicking the pin

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -16,22 +16,6 @@ angular.module('mainApp')
         
         function initApp(location){
             initializeMap(location.coords.latitude, location.coords.longitude);
-            // $http({
-            //     method : 'GET',
-            //     url : 'https://maps.googleapis.com/maps/api/geocode/json?address='+ address +'&key=AIzaSyDPemc_No5ae1c9PlCvchMq16ceDrb9Ed0'
-            // }).then(function successCallback(response) {
-            //     // this callback will be called asynchronously
-            //     // when the response is available
-            //     var location = response.data.results[0].geometry.location;
-            //     console.log(response);
-            //     initializeMap(location.lat, location.lng);
-            // }, function errorCallback(response) {
-            //     // called asynchronously if an error occurs
-            //     // or server returns response with an error status.
-            //     console.log('error');
-            //     console.log(response);
-            //     initializeMap(44.5403, -78.5463);
-            // });    
         }
         
         function initializeMap(lat, lng) {
@@ -46,11 +30,49 @@ angular.module('mainApp')
             dropPinOnMap(map, lat, lng);
         }
         
+        function addressToCoords(address){
+            $http({
+                method : 'GET',
+                url : 'https://maps.googleapis.com/maps/api/geocode/json?address='+ address +'&key=AIzaSyDPemc_No5ae1c9PlCvchMq16ceDrb9Ed0'
+            }).then(function successCallback(response) {
+                // this callback will be called asynchronously
+                // when the response is available
+                var location = response.data.results[0].geometry.location;
+                var locObject = {
+                    'lat' : location.lat,
+                    'lng' : location.lng
+                };
+                
+                return locObject;
+            }, function errorCallback(response) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+                console.log('Something went wrong. Returning default coords');
+                var locObject = {
+                    'lat' : 44.5403,
+                    'lng' : -78.5463
+                };
+                return locObject;
+            });    
+        }
+        
         function dropPinOnMap(map, lat, lng){
-            $scope.peopleInNeed.push(new google.maps.Marker({
+            var contentString = '<h1>Hello funky message</h1>';
+            var infowindow = new google.maps.InfoWindow({
+                content: contentString
+            });
+  
+            var marker = new google.maps.Marker({
                 position: new google.maps.LatLng(lat, lng),
-                map: map
-            }));
+                map: map,
+                icon: 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png'
+            });
+            
+            marker.addListener('click', function() {
+                infowindow.open(map, marker);
+            });
+            
+            $scope.peopleInNeed.push(marker);
         }
         
         
