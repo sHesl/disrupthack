714403c8de3c6654e559998b560c1a1846a7b73c
George Stefanis <info@georgestefanis.com>
2015-12-06 03:25:08 +0000

    removing the pin on your current location

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -25,7 +25,7 @@ angular.module('mainApp')
                 mapTypeId: google.maps.MapTypeId.ROADMAP
             };
             var map = new google.maps.Map(mapCanvas, mapOptions);
-            dropPinOnMap(map, lat, lng);
+            // dropPinOnMap(map, lat, lng); //this drops a pin on your current location
             getAllUsersList(map);
         }
         
