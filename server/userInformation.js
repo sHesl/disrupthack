9a883449010e30327c7dc83fad447fb8d8106aba
Dhaval Patel <mail@dhavalpatel.co.uk>
2015-12-06 01:25:04 +0000

    added the getalluser data and individual user methods

--- server/userInformation.js
+++ server/userInformation.js
@@ -1,17 +1,52 @@
 var http = require('http');
+var watsonUrl = 'http://disruptlondon2015.appspot.com/';
 
-exports.getAllUserInfo = function(req, res){
+//get all user data within a specific location
+exports.getAllUserInfo = function(req, response){
 	var location = req.params.location;
-	
-	console.log(location);
-	
-	res.end('{successful : true}');
+	var watsonRequest = watsonUrl + "getallusers";
+
+	http.get(watsonRequest, function(res) {
+		console.log("res.statusCode: ", res.statusCode);
+		var str = '';
+
+		res.on('data', function(chunk) {
+			str += chunk;
+		});
+
+		res.on('end', function() {
+			console.log("[evfService] Got user info: "+str);
+			response.end(str);
+		});
+
+	}).on('error', function(e) {
+		console.log("Got error: " + e.message);
+	});
+
+
 };
 
-exports.getUserData = function(req, res){
+exports.getUserData = function(req, response){
 	var userID = req.params.userID;
 	
-	console.log(userID);
-	
-	res.end('{successful : true}');
+	console.log("getUserData user id", userID);
+
+	//get data about a specific user
+	var watsonRequest = watsonUrl + "getuserdata/?userid=" + userID;
+
+	http.get(watsonRequest, function(res) {
+		console.log("res.statusCode: ", res.statusCode);
+
+		var str = '';
+		res.on('data', function(chunk) {
+			str += chunk;
+		});
+
+		res.on('end', function() {
+			console.log("[evfService] Got user info: "+str);
+			response.end(str)
+		});
+	}).on('error', function(e) {
+		console.log("Got error: " + e.message);
+	});
 };
